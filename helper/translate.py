084a62819e327d3dd919c3a06862cf1668e0f488
Kemo (decon-pc-71) <panzah.one@googlemail.com>
2023-12-01 14:56:20 +0100

    +

--- helper/translate.py
+++ helper/translate.py
@@ -4,6 +4,9 @@ import deepl, json
 auth_key = json.load(open('auth_key.json'))['deepl']
 translator = deepl.Translator(auth_key)
 
+t_glossary = None
+t_glossary_name = 'de-en'
+
 # Create an German to English glossary with two terms:
 entries = {
     'decore Base | UI fastly': 'decore Base | UI fastly',
@@ -19,18 +22,27 @@ entries = {
     'Erstellung': 'Build',
     }
 
-my_glossary = translator.create_glossary(
-    'My glossary',
-    source_lang='DE',
-    target_lang='EN-US',
-    entries=entries,
-)
+for glossary in translator.list_glossaries():
+    if glossary.name == t_glossary_name:
+        if translator.get_glossary_entries(glossary) != entries:
+            translator.delete_glossary(glossary)
+            t_glossary = None
+        else:
+            t_glossary = glossary
+    
+if not t_glossary:
+    t_glossary = translator.create_glossary(
+        t_glossary_name,
+        source_lang='DE',
+        target_lang='EN-US',
+        entries=entries,
+    )
 
 def translate(p_file):
     t_po_file = pofile(p_file)
     for entry in t_po_file:
         if entry.msgid and not entry.msgstr or entry.fuzzy:
-            t_text = translator.translate_text_with_glossary(entry.msgid, my_glossary, target_lang='EN-US').text
+            t_text = translator.translate_text_with_glossary(entry.msgid, t_glossary, target_lang='EN-US').text
             entry.msgstr = t_text
             entry.fuzzy = False
     t_po_file.save()
